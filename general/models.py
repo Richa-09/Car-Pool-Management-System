85c820f43d37c4f7721469913ef30fcb86c46a61
Richa Singh <singhricha234@gmail.com>
2020-03-11 18:10:49 +0530

    Minor Changes

--- general/models.py
+++ general/models.py
@@ -6,5 +6,4 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     occupation = models.TextField(max_length=50, default='No Occupation mentioned')
     phone = models.TextField(max_length=12, default='No phone no. provided')
-    
 
